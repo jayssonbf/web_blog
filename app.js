7970333849e0c326b3da455bc3c0df597c21c258
jbalbuena <jayssonbf@outlook.com>
2023-06-28 01:05:23 -0400

    added compose.ejs file, created a partials folder that contains the header and footer ejs files

--- app.js
+++ app.js
@@ -19,6 +19,24 @@ app.get('/', (req, res)=>{
   res.render('home', {homeContent: homeStartingContent});
 });
 
+app.get('/about', (req, res)=>{
+  res.render('about', {aboutText: aboutContent});
+});
+
+app.get('/contact', (req, res)=>{
+  res.render('contact', {contactText: contactContent});
+});
+
+app.get('/compose', (req, res)=>{
+  res.render('compose');
+});
+
+app.post('/compose', (req, res)=>{
+  
+  console.log(req.body.userInput);
+  res.redirect('/compose');
+});
+
 
 
 
